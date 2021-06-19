314d287d671f14696570e32bed68b06f1a46658a
Antonio Toriz <torizantonio15@gmail.com>
2021-06-18 20:29:04 -0700

    added Card component and Card styles

---
+++ src/components/Card.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import '../styles/Card.css';
+
+const Card = ({ title, description, price, image }) => {
+
+  return (
+    <div className="card">
+      <h4 className="card-title">{title}</h4>
+      {image ? <img src={image} alt={title} /> : <h2>Loading...</h2> }
+      
+      <div className="card-content">
+        <p>{description}</p>
+        <p className="price-label">£{price.toFixed(2)}</p>
+        <button className="btn" onClick={() => alert(`${title} has been added to your cart`)}>Add to card</button>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
