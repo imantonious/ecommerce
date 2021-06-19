7ce94ce33fa67a43eedb204389c276b9ff209a8d
Antonio Toriz <torizantonio15@gmail.com>
2021-06-18 21:23:45 -0700

    added header, searchbar, updated card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,18 +4,19 @@ import '../styles/Card.css';
 
 const Card = ({ title, description, price, image }) => {
 
-  return (
-    <div className="card">
-      <h4 className="card-title">{title}</h4>
-      {image ? <img src={image} alt={title} /> : <h2>Loading...</h2> }
-      
-      <div className="card-content">
-        <p>{description}</p>
-        <p className="price-label">£{price.toFixed(2)}</p>
-        <button className="btn" onClick={() => alert(`${title} has been added to your cart`)}>Add to card</button>
-      </div>
-    </div>
-  );
+	return (
+		<div className="card">
+			<h4 className="card-title">{title}</h4>
+
+			{image ? <img src={image} alt={title} /> : <h2>Loading...</h2> }
+
+			<div className="card-content">
+				<p>{description}</p>
+				<p className="price-label">£{price.toFixed(2)}</p>
+				<button className="btn" onClick={() => alert(`${title} has been added to your cart`)}>Add to card</button>
+			</div>
+		</div>
+	);
 };
 
 export default Card;
